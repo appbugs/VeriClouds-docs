dc08ef893106ab09ff1170d6b8dbcea8d48998ba
David Collins <david@designtrail.net>
2023-01-30 19:03:14 +0000

    Delete unused files

--- docs/conf.py
+++ docs/conf.py
@@ -172,3 +172,8 @@ epub_title = project
 
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
+
+
+# -- Other options -----------------------------------------------------------
+
+html_extra_path = 'extra'
--- docs/index_old.md
+++
@@ -1,7 +0,0 @@
-# Vericlouds API documentation
-
-Welcome to the Vericlouds API documentation.
-
-You can find the documentation for the CredVerify API [here](apidoc.md).
-
-
